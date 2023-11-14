210c94784ea58bde765756e37f6e773d626ec1bd
Kevin Chen <chenty0704@gmail.com>
2023-11-14 11:33:44 -0500

    Adds documentation.

--- Headers/System/JSON.h
+++ Headers/System/JSON.h
@@ -13,28 +13,36 @@ namespace describe = boost::describe;
 namespace json = boost::json;
 namespace mp11 = boost::mp11;
 
-template<semiregular T>
-requires is_class_v<T>
+template<semiregular T> requires is_class_v<T>
 T tag_invoke(const json::value_to_tag<T> &, const json::value &value) {
     T obj;
-    const auto &jsonObj = value.as_object();
+    const auto &_obj = value.as_object();
     using members = describe::describe_members<T, describe::mod_public>;
     mp11::mp_for_each<members>([&](auto member) {
         using M = remove_reference_t<decltype(obj.*member.pointer)>;
-        if (const auto it = jsonObj.find(member.name); it != jsonObj.cend())
+        if (const auto it = _obj.find(member.name); it != _obj.cend())
             obj.*member.pointer = json::value_to<M>(it->value());
     });
     return obj;
 }
 
+/// \brief Provides utilities for JSON conversion.
 namespace JSON {
+    /// \brief Returns a JSON representation of an object.
+    /// \tparam T The type of the object.
+    /// \param obj The object to serialize.
+    /// \return A JSON representation of the object.
     template<semiregular T>
-    inline string Serialize(const T &obj) {
+    string Serialize(const T &obj) {
         return json::serialize(json::value_from(obj));
     }
 
+    /// \brief Recovers an object from a JSON representation.
+    /// \tparam T The type of the object.
+    /// \param str The JSON represenation.
+    /// \return The recovered object from the JSON represenation.
     template<semiregular T>
-    inline T Deserialize(string_view str) {
+    T Deserialize(string_view str) {
         return json::value_to<T>(json::parse(str));
     }
 }
--- Headers/System/Reflection.h
+++ Headers/System/Reflection.h
@@ -2,5 +2,13 @@
 
 #include <boost/describe.hpp>
 
+/// \brief Describes a enum.
+/// \param E The type of the enum.
+/// \param ... The values of the enum.
 #define DEFINE_ENUM(E, ...) BOOST_DEFINE_ENUM_CLASS(E, __VA_ARGS__)
+
+/// \brief Describes a struct.
+/// \param C The type of the struct.
+/// \param Bases The bases of the struct.
+/// \param members The members of the struct.
 #define DESCRIBE_STRUCT(C, Bases, members) BOOST_DESCRIBE_STRUCT(C, Bases, members)
